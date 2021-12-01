73040cc083ad9408dbc7c09e61c6ad62ed6f5b09
Alicja Jagielska <alicja_dubai@wp.pl>
2021-12-01 10:58:04 +0100

    JSFromTheBeginning

--- js/script.js
+++ js/script.js
@@ -2,43 +2,39 @@
 'use strict';
 
 function titleClickHandler(event){
+
   const clickedElement = this;
   console.log('Link was clicked!');
-  event.preventDefault();
+  console.log(event);
 
-  /* [DONE] remove class 'active' from all article links  */
 
+  /* remove class 'active' from all article links  */
   const activeLinks = document.querySelectorAll('.titles a.active');
+  console.log(activeLinks);
 
   for(let activeLink of activeLinks){
-    activeLink.classList.remove('active');
+  activeLink.classList.remove('active');
+  console.log('ClassActiveRemoved');
   }
 
-  /* [IN PROGRESS] add class 'active' to the clicked link */
-  clickedElement.classList.add('active');
-  console.log('clickedElement:', clickedElement);
+   /* add class 'active' to the clicked link */
+   clickedElement.classList.add('active')
+   console.log('clickedElement:', clickedElement);
 
-
-
-   /* [DONE] remove class 'active' from all articles */
-  const activeArticles = document.querySelectorAll('.posts article.active');
+  /* remove class 'active' from all articles */
+   const activeArticles = document.querySelectorAll('.posts article.active');
 
   for(let activeArticle of activeArticles){
     activeArticle.classList.remove('active');
-    }
-    console.log('ActiveArticle');
-
+    console.log('activeArticleRemoved');
+  }
   /* get 'href' attribute from the clicked link */
+  const articleSelector = clickedElement.getAttribute('href');
+  console.log(articleSelector);
 
-
-  const articleSelector = clickedElement.getAttribute("href");
-    console.log(articleSelector);
-
-/* find the correct article using the selector (value of 'href' attribute) */
-
-const targetArticle = document.querySelector(articleSelector);
-console.log(targetArticle);
-
+  /* find the correct article using the selector (value of 'href' attribute) Czy ja tu nie musze wskazac jaka czesc html chce przeszukac?*/
+  const targetArticle = document.querySelector(articleSelector);
+  console.log(targetArticle)
 
   /* add class 'active' to the correct article */
   targetArticle.classList.add('active');
@@ -48,36 +44,44 @@ const links = document.querySelectorAll('.titles a');
 
 for(let link of links){
   link.addEventListener('click', titleClickHandler);
-
 }
-/*funcja generateTitleLinks*/
 
 const optArticleSelector = '.post',
-  optTitleSelector = '.post-title',
-  optTitleListSelector = '.titles';
+      optTitleSelector = '.post-title',
+      optTitleListSelector = '.titles';
+
 
 function generateTitleLinks(){
+    console.log('titleLinks');
 
-  /* remove contents of titleList  id?????*/
-const titleList = document.querySelector(optTitleListSelector)
+    /* remove contents of titleList */
+    const titleList = document.querySelector(optTitleListSelector);
+    console.log(titleList);
+    function clearMessages(){
+	  titleList.innerHTML = '';
 
-  function clearMessages() {
-    document.getElementById(titleList).innerHTML = '';
-    console.log('clear');
-  }
+}
+     /* for each article */
+    const articles = document.querySelectorAll(optArticleSelector);
 
+    let html = '';
 
-  /* for each article */
+    for(let article of articles){
+      /* get the article id */
+      const articleId = article.getAttribute('id');
+      console.log(articleId);
+      /* find the title element */
+      const articleTitle = article.querySelector(optTitleSelector).innerHTML;
+      console.log(articleTitle);
+      /* create HTML of the link */
+      const linkHTML = '<li><a href="#' + articleId + '"><span>' + articleTitle + '</span></a></li>';
+      console.log(linkHTML);
+      /* insert link into titleList */
+     html = html + linkHTML;
+     console.log(html);
+    }
 
-    /* get the article id */
-
-    /* find the title element */
-
-    /* get the title from the title element */
-
-    /* create HTML of the link */
-
-    /* insert link into titleList */
+  titleList.innerHTML = html;
 
 }
 
